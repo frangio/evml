ebaeb07cdf3e8b1c3eb78b33af2c83369c31c5da
Francisco Giordano <fg@frang.io>
2025-10-31 14:27:09 -0300

    add disassembly

--- src/bin/evml.rs
+++ src/bin/evml.rs
@@ -1,6 +1,27 @@
 use std::{env, fs::read_to_string};
 use anyhow::{anyhow, Result};
 use evml::{compile, parse, resolve, run};
+use revm::{bytecode::Bytecode, primitives::{Bytes, U256}};
+
+fn disasm(code: &[u8]) -> String {
+    let mut asm = String::new();
+    let bytecode = Bytecode::new_legacy(Bytes::copy_from_slice(code));
+    let mut iter = bytecode.iter_opcodes();
+    while let pos = iter.position() && pos < code.len() {
+        let opcode = iter.peek_opcode().unwrap();
+        let name = opcode.as_str();
+        asm.push_str(name);
+        let imm = opcode.info().immediate_size() as usize;
+        if imm > 0 {
+            let val = U256::from_be_slice(&code[pos+1..pos+1+imm]);
+            asm.push(' ');
+            asm.push_str(&val.to_string());
+        }
+        asm.push(' ');
+        iter.skip_to_next_opcode();
+    }
+    asm
+}
 
 fn main() -> Result<()> {
     let script_path = env::args().nth(1).ok_or(anyhow!("missing script argument"))?;
@@ -8,12 +29,11 @@ fn main() -> Result<()> {
     let block = parse(&source)?;
     let block = resolve(&block)?;
     let code = compile(&block);
+    let asm = disasm(&code);
     let result = run(&code);
 
     eprintln!("=== CODE ====");
-    for line in code.chunks(32) {
-        eprintln!("{}", line.iter().map(|b| format!("{b:02x?}")).collect::<String>());
-    }
+    eprintln!("{asm}");
 
     eprintln!("=== RESULT ==");
     match result {
