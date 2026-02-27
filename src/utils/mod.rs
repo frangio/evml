a2ff1d8d176e39429991f347659c60f32d9bf06d
Francisco Giordano <fg@frang.io>
2026-02-27 10:27:15 -0300

    rename set -> bitset

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,7 +1,7 @@
 pub mod shift_stack;
 pub mod exact_size_iter;
-pub mod set;
+pub mod bitset;
 
 pub use shift_stack::ShiftStack;
 pub use exact_size_iter::exact_size_chain;
-pub use set::BitSet;
+pub use bitset::BitSet;
